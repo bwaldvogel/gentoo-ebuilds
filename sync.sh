a3834c7b7d93caa8cffdaa316b42a7f910ada36b
Benedikt Waldvogel <mail@bwaldvogel.de>
2009-10-28 00:03:59 +0100

    exclude .gitignore on sync

--- sync.sh
+++ sync.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-time rsync -uarv --delete --delete-excluded --exclude='sync.sh' --exclude='.git' /usr/portage/local/my/ vps:0x11.net/htdocs/gentoo
+time rsync -uarv --delete --delete-excluded --exclude='sync.sh' --exclude='.git' --exclude='.gitignore' /usr/portage/local/my/ vps:0x11.net/htdocs/gentoo
